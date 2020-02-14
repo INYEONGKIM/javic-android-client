30f6e81fd1ffc3534e72791061eb3f4573065594
inyeongkim <hacerrumbo@gmail.com>
2020-02-14 16:09:25 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javic-android-client
-2020-1 capstone
+2020-1 capstone : Webview 기반 android chatbot client
+
+## Commit 이력
+- init:
+    - Web client 그대로 반영
+	- msg 입력시 화면이 확대되어 상단의 채팅 내용이 보이지 않는 문제 발생 (LG V40 기준)
+	- input 창이 상시 떠있는 문제 발생 (LG V40 기준)
---
+++ java/com/example/javic/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.javic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportMultipleWindows(false);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebView.loadUrl("http://{aws-ip}:{aws-chatbot-port}");
+    }
+}
